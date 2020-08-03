d08a7c8fdc2c401db7d15144e26fd30ce65de15e
Cyril Saidane <cs.saidane@gmail.com>
2020-08-03 19:57:52 -0400

    first few components were implemented in storybook and passed into main application

---
+++ src/components/DayList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import DayListItem from 'components/DayListItem';
+
+
+export default function DayList(props) {
+
+  const outputDays = props.days.map(day => {
+    return (<DayListItem
+      key={day.id} 
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === props.day}
+      setDay={props.setDay}  />);
+  });
+
+  return <ul>{outputDays}</ul>;
+}
